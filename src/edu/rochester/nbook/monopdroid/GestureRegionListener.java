feb6439ad6924f69059e5c6992d2b3512ab46905
Nate Book <nmbook@gmail.com>
2012-09-04 07:15:05 +0000

    F: Removed a class that I accidentally didn't remove. GestureRegionListener has moved to package .monopdroid.board.surface.
    git-svn-id: http://monopdroid.googlecode.com/svn/trunk@13 547c2936-0445-ec29-bcaf-865f2df33b66

--- src/edu/rochester/nbook/monopdroid/GestureRegionListener.java
+++
@@ -1,7 +0,0 @@
-package edu.rochester.nbook.monopdroid;
-
-public interface GestureRegionListener {
-    public void onRegionClick(Region region);
-
-    public void onRegionLongPress(Region region);
-}
