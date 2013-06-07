decf7f099d12cddf6be22adfbd7739fc0293b7ee
Nate Book <nmbook@gmail.com>
2013-06-06 20:18:30 -0400

    U: README. D: To respect .gitignore, deleted .classpath, .project and gen/.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ monopdroid
 ==========
 
 Monopoly Android App using the MONOPD servers.
+
+Moved from SVN and Google Code.
+
+This will be on the Google Play store when it's ready.
+
--- gen/edu/rochester/nbook/monopdroid/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.rochester.nbook.monopdroid;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
